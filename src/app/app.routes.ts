4e2511fdca052c53c5f091bfa16b5f55a6fe6f49
Joaquin Telleria <joaquintelleria@gmail.com>
2025-01-22 21:21:40 +0100

    add header and footer components, implement routing for homepage and about pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'about', component: AboutComponent },
+];
